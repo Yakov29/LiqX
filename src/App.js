866c244e0f1a4831c78b4bdd9d280f11501e4270
ZOVUT YAKOV <108018683+Yakov29@users.noreply.github.com>
2025-11-11 13:09:55 +0200

    o4ko

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Login from "./pages/Login/Login";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import Download from "./pages/Download/Download";
-import Forum from "./pages/Forum/Forum";
 
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/download" element={<Download />}></Route>
-        <Route path="/forum" element={<Forum />}></Route>
 
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
